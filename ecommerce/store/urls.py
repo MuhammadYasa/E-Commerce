4cfa98fdec8ccb5778e9b0f14a69a8b90076ed62
MuhammadYasa <66125818+MuhammadYasa@users.noreply.github.com>
2020-08-25 13:38:48 +0700

    setting django & configure templates

---
+++ ecommerce/store/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views # maksudnya adalah dia mengimport views ke dalam url
+
+urlpatterns = {
+    path('', views.store, name='store'), # '' maksudnya itu home
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout')
+}
